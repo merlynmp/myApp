e3eac672eb6d5ad5fb9aaa7d1389be14b8ca7c08
Merlyn Pothen <merlynpothencollege@gmail.com>
2024-05-25 23:01:50 -0400

    update to receive stock data

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
- const dotenv = require('dotenv');
- dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
-console.log(process.env.TEST);
+const axios = require('axios');
+
+axios
+  	.get(process.env.API_URL, {
+    		json: true,
+    		headers: { "User-Agent": "request" },
+    		params: {
+      			//function: "aggregates",
+      			//stocksTicker: "APPL",
+      			//multiplier: "5min",
+                //timespan: "day",
+                //from: "2024-01-01",
+                //to: "2024-05-01",
+      			//apikey: process.env.API_KEY,
+    		},
+  	})
+  	.then((response) => {
+    		console.log(response.data);
+  	})
+  	.catch((error) => {
+    		console.log("This is an error");
+  	});
