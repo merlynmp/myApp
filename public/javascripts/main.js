638b33bd7e2fcd138c4be0b8c02e2bf44399d59b
Merlyn Pothen <merlynpothencollege@gmail.com>
2024-06-04 22:49:31 -0400

    Applied low and high

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,19 +7,31 @@ function myFunction(e) {
   }
   
   function getStocks(input) {
-    populateTable([]);
+    /*populateTable([]);*/
     
-    /*axios
+    axios
       .get(`/api/prices/${input}`, {
         json: true,
       })
       .then((response) => {
-        console.log(response.data);
         populateTable(response.data);
+        console.log(response.data);
+        const highValues = [];
+        const lowValues = [];
+
+        response.data.results.forEach( result => {
+          highValues.push(result.h);
+          lowValues.push(result.l);
+        });
+
+        console.log(highValues);
+        console.log(lowValues)
+
+        
       })
       .catch((error) => {
         console.error(error);
-      });*/
+      });
 
 
     //setTimeout(arguments.callee, 5000);
@@ -28,12 +40,32 @@ function myFunction(e) {
   function populateTable(data) {
     const tableBody = document.getElementById("table-body");
     tableBody.innerHTML = "";
-    const array = generateRandomArray(generateRandomNumber(10));
+    /*const array = generateRandomArray(generateRandomNumber(10));*/
+    const array = data;
     const template = document.getElementById("table-row");
     const templateCell = document.getElementById("table-cell");
     const row = template.content.querySelector("tr");
     const cell = templateCell.content.querySelector("td");
-    array.forEach((element) => {
+    i = 0;
+    array.results.forEach(result => {
+      const a = document.importNode(row, true);
+      let myCell = document.importNode(cell, true);
+      i++;
+      myCell.innerHTML = i;
+      a.appendChild(myCell);
+
+      myCell = document.importNode(cell, true);
+      myCell.innerHTML = result.l;
+      a.appendChild(myCell);
+
+      myCell = document.importNode(cell, true);
+      myCell.innerHTML = result.h;
+      a.appendChild(myCell);
+  
+      tableBody.appendChild(a);
+  
+    })
+    /*array.forEach((element) => {
       const a = document.importNode(row, true);
       let myCell = document.importNode(cell, true);
       myCell.innerHTML = element.name;
@@ -48,7 +80,7 @@ function myFunction(e) {
       a.appendChild(myCell);
   
       tableBody.appendChild(a);
-    });
+    });*/
     document.getElementById("table").classList.remove("d-none");
   }
   
